66f45cec78a42884c9e204ebab65032ae2b6d286
“melvin” <melvin@gmail.com>
2024-10-28 21:40:28 +0100

    Commit inicial

---
+++ Cajero.java
@@ -0,0 +1,32 @@
+public class Cajero{
+    String nombreDelCliente;
+    int balanceDelCliente;
+    boolean cuentaDeAhorro;
+    public Cajero(String elegirNombreDelCliente, boolean elegirCuentaDeAhorro){
+        nombreDelCliente = elegirNombreDelCliente;
+        balanceDelCliente = 0;
+        cuentaDeAhorro = elegirCuentaDeAhorro;
+    }
+
+    public String getNombreCliente(){
+        return nombreDelCliente;
+    }
+
+    public int getBalanceCliente(){
+        return balanceDelCliente;
+    }
+    public boolean getCuentaDeAhorro(){
+        return cuentaDeAhorro;
+    }
+    public void introducirDinero(int cantidadDeDineroIntroducida){
+        int totalCantidadDeDineroIntroducida = cantidadDeDineroIntroducida;
+        if(totalCantidadDeDineroIntroducida >= 5){
+            balanceDelCliente = balanceDelCliente + cantidadDeDineroIntroducida;
+            System.out.println("Has ingresado " + totalCantidadDeDineroIntroducida + " euros");
+            System.out.println("Tu saldo actual es de " + balanceDelCliente + " euros");
+        }else{
+            System.out.println("La cantidad mínima de ingreso son 5 euros");
+        }
+    }
+}
+
